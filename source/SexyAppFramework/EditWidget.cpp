57efddc9c6a4762eec853d74ddf15dbf61dd914e
regeneration5775 <regeneration5775@gmail.com>
2026-03-14 16:22:37 +0900

    Fixed EditWidget language problem.

--- source/SexyAppFramework/EditWidget.cpp
+++ source/SexyAppFramework/EditWidget.cpp
@@ -1,4 +1,4 @@
-#include "EditWidget.h"
+﻿#include "EditWidget.h"
 #include "SysFont.h"
 #include "WidgetManager.h"
 #include "SexyAppBase.h"
@@ -263,7 +263,7 @@ bool EditWidget::IsPartOfWord(SexyChar theChar)
 	return (((theChar >= _S('A')) && (theChar <= _S('Z'))) ||
 			((theChar >= _S('a')) && (theChar <= _S('z'))) ||
 			((theChar >= _S('0')) && (theChar <= _S('9'))) ||
-			(((unsigned int)theChar >= (unsigned int)(L'?')) && ((unsigned int)theChar <= (unsigned int)(L'�'))) ||
+			(((unsigned int)theChar >= 0xC0) && ((unsigned int)theChar <= 0xFF)) || // This checks if the character is bewteen À to ÿ, but fails miserably in compilers of some languages.
 			(theChar == _S('_')));
 }
 
